c41296ac325fd2d17e5910266fe4a85abf158696
gnilk <kling.fredrik@gmail.com>
2019-03-19 08:25:16 +0100

    Patch from krikkit

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git clone https://github.com/ocornut/imgui.git dep/imgui
-#git clone https://github.com/glfw/glfw.git dep/glfw
-
+git clone https://github.com/glfw/glfw.git dep/glfw
+git clone https://github.com/BalazsJako/ImGuiColorTextEdit.git dep/ImGuiColorTextEdit
